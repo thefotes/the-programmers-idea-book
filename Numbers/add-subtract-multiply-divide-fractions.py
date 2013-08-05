ce8afcb1d892b9251c58879f4b2e2694aa3c9c11
Peter <peterfoti@me.com>
2013-08-05 15:21:59 -0400

    add-subtract-multiply-divide-fractions.py

--- Numbers/add-subtract-multiply-divide-fractions.py
+++ Numbers/add-subtract-multiply-divide-fractions.py
@@ -1,4 +1,5 @@
-"""Description: This project involves working with fractions. How do you add 1⁄3 + 1⁄5? Create a program that first asks the user which operation they want to do: add, subtract, multiply or divide and then asks for 1, 2 or more fractions to work with. The program prints out the result."""
+"""Description: This project involves working with fractions. How do you add 1⁄3 + 1⁄5? Create a program that first asks the user which operation they want to do: add, subtract, multiply or divide and then asks for 1, 2 or more fractions to work with. 
+The program prints out the result."""
 """In this project I opted for the built in fractions module. At first I attempted to write my own class for the solving but when I stumbled across this module I could hear the bells going off 'work smarter, not harder'. So I did."""
 
 from fractions import Fraction
